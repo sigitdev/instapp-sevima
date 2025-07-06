abc60eb14ea2064190c3513988822691c33ce48c
Sigit Wahyu Wibowo <sigitwahyu05@gmail.com>
2025-07-06 23:47:29 +0700

    style(login) : add login page, layouting and add style

--- view/layout/header.php
+++ view/layout/header.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insta App</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="<?= $this->base_url('/asset/bootstrap/css/bootstrap.min.css')  ?>">
+    <link rel="stylesheet" href="<?= $this->base_url('/asset/style.css')  ?>">
 </head>
 <body>
--- view/login.php
+++ view/login.php
@@ -1,12 +1,20 @@
 <?php include 'layout/header.php' ?>
 <!-- content disini -->
 <div class="container">
-    ini halaman lagi
+    <div class="icon-app">
+        <i class="bi bi-chat-left-heart-fill"></i>
+    </div>
+    <div class="login-app-name">
+        Insta App
+    </div>
+
+    <a href="#" class="btn-register shadow">Create Account</a>
+    <div class="text-center m-3">Or</div>
+    <a href="<?= $this->base_url('/home') ?>" class="link-login">Already have account</a>
 </div>
 
 <?php include 'layout/lib.php' ?>
 <script>
-    $(document).ready(function(){
-    })
+    $(document).ready(function() {})
 </script>
 <?php include 'layout/footer.php' ?>
